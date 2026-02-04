e7acf34effa550ce08aec572f64e74a39da9f624
AllMighty-57 <ecainepetty@gmail.com>
2026-02-04 14:40:50 -0700

    Updated Arena / Enemy Behavior

--- MyHeroArena/Assets/Scripts/EnemyBehavior.cs
+++ MyHeroArena/Assets/Scripts/EnemyBehavior.cs
@@ -1,13 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform PatrolRoute;
+     
+    public List<Transform> Locations;
+
+    private int _locationIndex = 0;
     
-    void OnTriggerEnter(Collider other)
+    private NavMeshAgent _agent;
+    void Start()
     {
-        //2 
+        _agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+
+        MoveToNextPatrolLocation();
+    }
+
+    void Update()
+    {
+        if (_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in PatrolRoute)
+        {
+            Locations.Add(child);
+        }
+    }
+
+
+    void MoveToNextPatrolLocation()
+    {
+        if (Locations.Count == 0)
+            return; 
+
+        _agent.destination = Locations[_locationIndex].position;
+
+        _locationIndex = (_locationIndex + 1) % Locations.Count;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {        
         if (other.name == "Player")
         {
             Debug.Log("Player detected - attack!");
@@ -16,11 +59,10 @@ public class EnemyBehavior : MonoBehaviour
 
     
     void OnTriggerExit(Collider other)
-    {
-        // 4
+    {        
         if (other.name == "Player")
         {
-            Debug.Log("Player out of range, resume patrol");
+            Debug.Log("Player out of range, resuming patrol");
         }
     }
 }
